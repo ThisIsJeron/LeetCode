72392e64a1a0e7913e7cd0dfb1904cfbabe5253a
Jeron Wong <ThisIsJeron@users.noreply.github.com>
2019-06-03 16:37:54 -0700

    removed solns

--- bethany/191_Number_of_1_Bits_Easy.java
+++
@@ -1,9 +0,0 @@
-public class Solution {
-    // you need to treat n as an unsigned value
-    public int hammingWeight(int n) {
-        return Integer.bitCount(n);
-    }
-}
-//Results:
-//Runtime: 0 ms, faster than 100.00% of Java online submissions for Number of 1 Bits.
-//Memory Usage: 32.1 MB, less than 100.00% of Java online submissions for Number of 1 Bits.
--- bethany/709_To_Lower_Case_Easy.java
+++
@@ -1,9 +0,0 @@
-iclass Solution {
-    public String toLowerCase(String str) {
-        return str.toLowerCase();
-    }
-}
-
-//Results
-//Runtime: 0 ms, faster than 100.00% of Java online submissions for To Lower Case.
-//Memory Usage: 35.4 MB, less than 92.41% of Java online submissions for To Lower Case.i
